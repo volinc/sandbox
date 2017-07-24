2390b753c7385c88612ea2078dbe8739b42b5776
Ruslan Tatarinov <r.tatarinov@taxys.ru>
2017-07-24 17:55:12 +0300

    ...

---
+++ EfTest/EF6Test/Data/DriverVehicleData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EF6Test.Data
+{
+    public class DriverVehicleData
+    {
+        public long DriverId { get; set; }
+
+        public Guid VehicleId { get; set; }
+
+        public virtual ICollection<long> Drivers { get; } = new HashSet<long>();
+
+        public virtual ICollection<Guid> Vehicles { get; } = new HashSet<Guid>();
+    }
+}
