39cca8cc0c868fe9fc758a00ac4f5b718c1732ac
r.tatarinov <vol.inc@gmail.com>
2017-08-07 23:19:35 +0300

    Initial

---
+++ XamarinFormsTest/XamarinFormsTest/XamarinFormsTest/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace XamarinFormsTest.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
