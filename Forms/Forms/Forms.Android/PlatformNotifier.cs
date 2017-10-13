6ccf3103e30342e37c3574ee480511b857237ddc
r.tatarinov <r.tatarinov@taxys.ru>
2017-10-13 17:50:06 +0300

    ...

---
+++ Forms/Forms/Forms.Android/PlatformNotifier.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Forms.Infrastructure;
+using Plugin.CurrentActivity;
+
+namespace Forms.Droid
+{
+    internal class PlatformNotifier : IPlatformNotifier
+    {
+        public async Task<bool> ShowDialogAsync(string message)
+        {
+            var mainActivity = CrossCurrentActivity.Current.Activity;
+
+            //https://stackoverflow.com/questions/39479508/show-dialog-activity-over-another-app-from-background
+
+            await Task.Delay(0);
+        }
+    }
+}
