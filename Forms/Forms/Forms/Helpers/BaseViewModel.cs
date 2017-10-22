615f774bd90ac0f6e940da6e35394d11904412cb
r.tatarinov <r.tatarinov@taxys.ru>
2017-10-22 04:10:48 +0300

    ...

--- Forms/Forms/Forms/Helpers/BaseViewModel.cs
+++ Forms/Forms/Forms/Helpers/BaseViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
 
 namespace Forms.Helpers
 {
@@ -9,5 +11,38 @@ namespace Forms.Helpers
         }
 
         public virtual bool IsBusy { get; set; }
+
+        public T GetPersistentValue<T>([CallerMemberName]string propertyName = "")
+        {
+            var key = StoreHelper.GeneratePropertyKey(GetType(), propertyName);
+            TryGetPersistentValue(key, out T value);
+
+            return value;
+        }
+
+        public void SetPersistentValue<T>(T value, [CallerMemberName] string propertyName = "", Action onChanged = null)
+        {
+            var key = StoreHelper.GeneratePropertyKey(GetType(), propertyName);
+            TryGetPersistentValue(key, out T storedValue);
+
+            if (SetProperty(ref storedValue, value, propertyName, onChanged))
+                Application.Current.Properties[key] = value;
+        }
+
+        private static bool TryGetPersistentValue<T>(string key, out T value)
+        {
+            if (Application.Current.Properties.TryGetValue(key, out var objectValue))
+            {
+                if (objectValue is T storedValue)
+                {
+                    value = storedValue;
+                    return true;
+                }
+            }
+
+            value = default(T);
+
+            return false;
+        }
     }
 }
