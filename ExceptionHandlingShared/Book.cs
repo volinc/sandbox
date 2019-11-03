bcac6785b0d6290d753591f167c24349d58b40f8
r.tatarinov <r.tatarinov@taxys.ru>
2019-11-03 22:54:01 +0300

    ...

---
+++ ExceptionHandlingShared/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace ExceptionHandlingShared
+{
+    using System;
+
+    public class Book
+    {
+        public void Throw() => throw new InvalidOperationException("Very invalid op");
+    }
+}
