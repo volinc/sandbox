5d43555dad0dfa603b298bd8f1f4b90cb4bda697
Ruslan Tatarinov <r.tatarinov@taxys.ru>
2019-11-06 01:49:17 +0300

    ...

---
+++ MessagePackTest/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace MessagePackTest
+{
+    using MessagePack;
+    using MessagePack.Resolvers;
+    using System;
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            var book = new Book("abc");
+            var bytes = MessagePackSerializer.Serialize(book, ContractlessStandardResolver.Instance);
+            book = MessagePackSerializer.Deserialize<Book>(bytes, ContractlessStandardResolver.Instance);
+            Console.WriteLine(book.Author);
+        }
+    }
+
+    public class Book
+    {
+        public Book(string author)
+        {
+            Author = author;
+        }
+
+        public string Author { get; }
+    }
+}
