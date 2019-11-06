f2e1330bf44a3b053cfdfe885806461be0c42b9c
r.tatarinov <r.tatarinov@taxys.ru>
2019-11-06 04:03:03 +0300

    ...

--- MessagePackTest/Program.cs
+++ MessagePackTest/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace MessagePackTest
 {
     using MessagePack;
+    using MessagePack.Formatters;
     using MessagePack.Resolvers;
     using System;
 
@@ -8,25 +9,39 @@
     {
         private static void Main()
         {
-            var book = Book.Create("abc");
-            var bytes = MessagePackSerializer.Serialize(book, ContractlessStandardResolver.Instance);
+            var book = Book.Create(BookState.None, "abc", 1);
+            var bytes = MessagePackSerializer.Serialize(book, ContractlessStandardResolverAllowPrivate.Instance);
             book = MessagePackSerializer.Deserialize<Book>(bytes, ContractlessStandardResolver.Instance);
-            Console.WriteLine(book.Author);
+            Console.WriteLine(book);
         }
+    }
+    
+    public enum BookState
+    {
+        None = 0,
+        Published = 10
     }
 
     public class Book
     {
-        public Book(string author)
+        public static readonly Book Complete = new Book(BookState.Published, "abc", 1);
+        public static readonly Book New = new Book(BookState.None, "cba", 1);
+
+        private Book(BookState state, string author, int count)
         {
+            State = state;
             Author = author;
+            Count = count;
         }
+
+        public BookState State { get; }
 
         public string Author { get; }
 
-        public static Book Create(string author)
-        {
-            return new Book(author);
-        }
+        public int Count { get; }
+
+        public static Book Create(BookState state, string author, int count) => new Book(state, author, count);
+
+        public override string ToString() => $"{State} {Author}";
     }
 }
