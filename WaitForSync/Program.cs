a91e55d037d32eaf2f219ca4ea77c75e706193eb
r.tatarinov <r.tatarinov@taxys.ru>
2020-02-03 01:35:54 +0300

    ...

---
+++ WaitForSync/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace WaitForSync
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        public static async void Main()
+        {
+            await Task.Delay(100);
+        }
+    }
+}
---
+++ WaitForSync/Synchronizer.cs
@@ -0,0 +1,31 @@
+﻿namespace WaitForSync
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class Synchronizer
+    {
+        private readonly SemaphoreSlim semaphoreSlim = new SemaphoreSlim(0, 1);
+        private int executing;
+
+        public Synchronizer()
+        {
+            TaskScheduler.FromCurrentSynchronizationContext
+        }
+
+        public async Task Sync(string value = null)
+        {            
+            if (Interlocked.CompareExchange(ref executing, 1, 0) != 0)                
+                return;
+            
+            try
+            {
+                await Task.Delay(5000);
+            }
+            finally
+            {
+                executing = 0;
+            }
+        }
+    }
+}
